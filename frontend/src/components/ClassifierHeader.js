d43bf1d34c97923642618cae6e4ac1467046f23b
17bsh2 <17bsh2@queensu.ca>
2023-03-22 14:57:44 -0400

    Survey with weird formatting

--- frontend/src/components/ClassifierHeader.js
+++ frontend/src/components/ClassifierHeader.js
@@ -5,10 +5,10 @@ import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material';
 import { TextField, createTheme } from '@mui/material';
 import { Input, ariaLabel } from '@mui/material';
-//import Button from '@mui/material/Button';
+import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-//import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 
 const ClassifierHeader = () => {
 const theme = createTheme();
@@ -38,18 +38,18 @@ const theme = createTheme();
                     Select answers to the question prompts as best as possible, these answers will determine your results!
                 </Typography>
             </Box>
-            
-            Most Relevant (24 features): 
-
 
             <Box padding={2}>
             <Typography
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Has any first-degree relative had cancer? 
+                    Basal cell skin cancers are not included. 
+                    First-degree relatives include parents, full-siblings, and children.
+                     Half-siblings are not included. (Yes/No)
                 </Typography>
-            <TextField id="fh_cancer" label="1" variant="standard" />
+            <TextField id="fh_cancer" label="Yes/No" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -57,27 +57,35 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Enter the number of first-degree relatives with ovarian, fallopian tube, or peritoneal 
+                    cancer. Ovarian summary cancers include ovarian, fallopian tube and peritoneal cancer.
                 </Typography>
-                <TextField id="ovarsumm_fh_cnt" label="1" variant="standard" />
+                <TextField id="ovarsumm_fh_cnt" label="#" variant="standard" />
     </Box>
     <Box padding={2}>
     <Typography
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Breast cancer family history in first-degree relatives. Includes parents, full-siblings, and children.
+                   
+                    Indicate EITHER: Yes/No/Male Relative Only/Possibly - Relative or Cancer Type Not Clear
                 </Typography>
-                <TextField id="breast_fh" label="1" variant="standard" />
+                <TextField id="breast_fh" label="Yes/No/Male Relative Only/Possibly - Relative or Cancer Type Not Clear" variant="standard" />
     </Box>
+
+
     <Box padding={2}>
     <Typography
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Ovarian, fallopian tube, or peritoneal cancers family history in first-degree relatives.
+                     Includes parents, full-siblings, and children. Ovarian summary cancers include ovarian, fallopian tube and 
+                     peritoneal cancers. Indicate EITHER: 
+                     Yes - Immediate Family Member/No/Possibly - Relative or Cancer Type Not Clear
                 </Typography>
-                <TextField id="ovarsumm_fh" label="1" variant="standard" />
+                <TextField id="ovarsumm_fh" label="Yes - Immediate Family Member/No/Possibly - Relative or Cancer Type Not Clear" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -85,9 +93,9 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Indicate how many first-degree relatives have breast cancer.
                 </Typography>
-                <TextField id="breast_fh_cnt" label="1" variant="standard" />
+                <TextField id="breast_fh_cnt" label="#" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -95,9 +103,10 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Indicate the diagnosis age of the youngest first-degree relative diagnosed with breast cancer.
+                    If not applicable, enter .N
                 </Typography>
-                <TextField id="breast_fh_age" label="1" variant="standard" />
+                <TextField id="breast_fh_age" label="# OR .N" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -105,9 +114,11 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Indicate the diagnosis age of the youngest first-degree relative diagnosed with ovarian, fallopian tube,
+                     or peritoneal cancer. Ovarian summary cancers include ovarian, fallopian tube and peritoneal cancers.
+                     If not applicable, enter .N
                 </Typography>
-                <TextField id="ovarsumm_fh_age" label="1" variant="standard" />
+                <TextField id="ovarsumm_fh_age" label="# OR .N" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -115,9 +126,10 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Have you ever been told by a doctor that you had a benign ovarian tumor/cyst?
+
                 </Typography>
-                <TextField id="benign_ovcyst" label="1" variant="standard" />
+                <TextField id="benign_ovcyst" label="Yes/No/Decline to Answer" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -125,9 +137,9 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                Have you ever been told by a doctor that you had endometriosis?
                 </Typography>
-                <TextField id="endometriosis" label="1" variant="standard" />
+                <TextField id="endometriosis" label="Yes/No/Decline to Answer" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -135,9 +147,9 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Have you ever been told by a doctor that you had uterine fibroid tumors?
                 </Typography>
-                <TextField id="uterine_fib" label="1" variant="standard" />
+                <TextField id="uterine_fib" label="Yes/No/Decline to Answer" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -145,9 +157,10 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                     During the past three years, have you had a mammogram?
+
                 </Typography>
-                <TextField id="mammo_history" label="1" variant="standard" />
+                <TextField id="mammo_history" label="Yes/No/Decline to Answer" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -155,9 +168,9 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    During the past three years, have you had a pap smear?
                 </Typography>
-                <TextField id="papsmear_history" label="1" variant="standard" />
+                <TextField id="papsmear_history" label="Yes/No/Decline to Answer" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -165,9 +178,9 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                     During the past three years, have you had a pelvic examination?
                 </Typography>
-                <TextField id="pelvic_history" label="1" variant="standard" />
+                <TextField id="pelvic_history" label="Yes/No/Decline to Answer" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -175,9 +188,10 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    During the past three years, have you had an ultrasound or scan of your ovaries?
+
                 </Typography>
-                <TextField id="usound_history" label="1" variant="standard" />
+                <TextField id="usound_history" label="Yes/No/Decline to Answer" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -185,9 +199,10 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    During the past three years, have you had a blood test for ovarian cancer, for example CA-125?
+
                 </Typography>
-                <TextField id="ca125_history" label="1" variant="standard" />
+                <TextField id="ca125_history" label="Yes/No/Decline to Answer" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -195,9 +210,10 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Have you ever been told by a doctor that you had benign or fibrocystic breast disease?
+
                 </Typography>
-                <TextField id="bbd" label="1" variant="standard" />
+                <TextField id="bbd" label="Yes/No/Decline to Answer" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -205,9 +221,16 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Have you ever had one or both of your ovaries removed?
+                    Indicate EITHER:
+                    Ovaries Not Removed
+                    One Ovary - Partial
+                    One Ovary - Total
+                    Both Ovaries - Partial
+                    Both Ovaries - Total
+                    Don't Know
                 </Typography>
-                <TextField id="ovariesr_f" label="1" variant="standard" />
+                <TextField id="ovariesr_f" label="" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -215,9 +238,9 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Enter your age.
                 </Typography>
-                <TextField id="age" label="1" variant="standard" />
+                <TextField id="age" label="#" variant="standard" />
     </Box>
         
     <Box padding={2}>
@@ -225,9 +248,9 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    What is your weight in pounds?
                 </Typography>
-                <TextField id="weight_f" label="1" variant="standard" />
+                <TextField id="weight_f" label="#" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -235,9 +258,9 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    What is your height in inches?
                 </Typography>
-                <TextField id="height_f" label="1" variant="standard" />
+                <TextField id="height_f" label="# inches" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -245,9 +268,9 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Did you have ovaries at this height and weight?
                 </Typography>
-                <TextField id="ovary_trial_flag" label="1" variant="standard" />
+                <TextField id="ovary_trial_flag" label="Yes/No" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -255,9 +278,17 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Over the past three months, how much have you beenbothered by abdominal, pelvic, or genital pain?
+                    Indicate either:
+                    1 - Not at all
+                    2 - A little
+                    3 A lot
+                    4 Somewhere in between a little and a lot
+                    7 Refuse to answer
+                    8 Not Ascertained
+                    9 Don't Know
                 </Typography>
-                <TextField id="PAIAPG3M_A" label="1" variant="standard" />
+                <TextField id="PAIAPG3M_A" label="1/2/3/4/5/6/7/8/9" variant="standard" />
     </Box>
 
     <Box padding={2}>
@@ -265,7 +296,15 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Over the last two weeks, how often have you been bothered by poor appetite or overeating?
+                    Indicate either:
+                    1 - Not at all
+                    2 - Several days
+                    3 - More than half the days
+                    4 - Nearly every day
+                    7 - Refused
+                    8 - Not Ascertained
+                    9 - Don't Know
                 </Typography>
                 <TextField id="PHQ85_A" label="1" variant="standard" />
     </Box>
@@ -275,9 +314,19 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Select answers to the question prompts as best as possible, these answers will determine your results!
+                    Do you have difficulty walking or climbing steps? Would you say no difficulty,
+                    some difficulty, a lot of difficulty, or you cannot do this at all?
+                    Indicate either:
+                    1 - No difficulty
+                    2 - Some difficulty
+                    3 - A lot of difficulty
+                    4 - Cannot do at all
+                    7 - Refused
+                    8 - Not Ascertained
+                    9 - Don't Know
+
                 </Typography>
-                <TextField id="DIFF_A" label="1" variant="standard" />
+                <TextField id="DIFF_A" label="1/2/3/4/5/6/7/8/9" variant="standard" />
     </Box>
         </Grid>
     );
