6e9351fcba23502a7c6fd12d36daa5472d9329d4
17bsh2 <17bsh2@queensu.ca>
2023-03-22 16:42:37 -0400

    Committing Nutrition, Calendar draft pages

--- frontend/src/components/ClassifierHeader.js
+++ frontend/src/components/ClassifierHeader.js
@@ -44,7 +44,6 @@ const theme = createTheme();
 
   const handleSubmit = async (e) => {
 
-    
 }
 
   const handleClick = (event) => {
@@ -104,10 +103,12 @@ const theme = createTheme();
                     color={theme.palette.text.secondary}
                     variant='h8'
                 >
-                    Has any first-degree relative had cancer? 
+                    Has any first-degree relative had cancer?
+                    <Typography></Typography> 
                     Basal cell skin cancers are not included. 
                     First-degree relatives include parents, full-siblings, and children.
-                     Half-siblings are not included. (Yes/No)
+                     Half-siblings are not included.
+                     <Typography></Typography>
                 </Typography>
             <TextField id="fh_cancer" label="Yes/No" value={question1}
             onChange={(e) => setQuestion1(e.target.value)} variant="standard" />
@@ -122,6 +123,7 @@ const theme = createTheme();
                 >
                     Enter the number of first-degree relatives with ovarian, fallopian tube, or peritoneal 
                     cancer. Ovarian summary cancers include ovarian, fallopian tube and peritoneal cancer.
+                    <Typography></Typography>
                 </Typography>
                 <TextField id="ovarsumm_fh_cnt" label="#"
                 value={question2}
@@ -133,8 +135,13 @@ const theme = createTheme();
                     variant='h8'
                 >
                     Breast cancer family history in first-degree relatives. Includes parents, full-siblings, and children.
-                   
-                    Indicate EITHER: Yes/No/Male Relative Only/Possibly - Relative or Cancer Type Not Clear
+                    <Typography></Typography>
+                    Indicate EITHER:
+                    <Typography></Typography> Yes
+                    <Typography></Typography>No
+                    <Typography></Typography>Male Relative Only
+                    <Typography></Typography>Possibly - Relative or Cancer Type Not Clear
+                    <Typography></Typography>
                 </Typography>
                 <TextField id="breast_fh" 
                 label="Yes/No/Male Relative Only/Possibly - Relative or Cancer Type Not Clear"
@@ -150,8 +157,16 @@ const theme = createTheme();
                 >
                     Ovarian, fallopian tube, or peritoneal cancers family history in first-degree relatives.
                      Includes parents, full-siblings, and children. Ovarian summary cancers include ovarian, fallopian tube and 
-                     peritoneal cancers. Indicate EITHER: 
-                     Yes - Immediate Family Member/No/Possibly - Relative or Cancer Type Not Clear
+                     peritoneal cancers. 
+                     <Typography></Typography>
+                     Indicate EITHER: 
+                     <Typography></Typography>
+                     Yes - Immediate Family Member
+                     <Typography></Typography>
+                     No
+                     <Typography></Typography>
+                     Possibly - Relative or Cancer Type Not Clear
+                     <Typography></Typography>
                 </Typography>
                 <TextField id="ovarsumm_fh"
                  label="Yes - Immediate Family Member/No/Possibly - Relative or Cancer Type Not Clear"
@@ -165,6 +180,7 @@ const theme = createTheme();
                     variant='h8'
                 >
                     Indicate how many first-degree relatives have breast cancer.
+                    <Typography></Typography>
                 </Typography>
                 <TextField id="breast_fh_cnt" label="#"
                 value={question5}
@@ -177,7 +193,9 @@ const theme = createTheme();
                     variant='h8'
                 >
                     Indicate the diagnosis age of the youngest first-degree relative diagnosed with breast cancer.
+                    <Typography></Typography>
                     If not applicable, enter .N
+                    <Typography></Typography>
                 </Typography>
                 <TextField id="breast_fh_age" label="# OR .N" 
                 value={question6}
@@ -191,7 +209,10 @@ const theme = createTheme();
                 >
                     Indicate the diagnosis age of the youngest first-degree relative diagnosed with ovarian, fallopian tube,
                      or peritoneal cancer. Ovarian summary cancers include ovarian, fallopian tube and peritoneal cancers.
+                    
+                     <Typography></Typography>
                      If not applicable, enter .N
+                     <Typography></Typography>
                 </Typography>
                 <TextField id="ovarsumm_fh_age" label="# OR .N"
                 value={question7}
@@ -204,6 +225,7 @@ const theme = createTheme();
                     variant='h8'
                 >
                     Have you ever been told by a doctor that you had a benign ovarian tumor/cyst?
+                    <Typography></Typography>
 
                 </Typography>
                 <TextField id="benign_ovcyst" label="Yes/No/Decline to Answer"
@@ -217,6 +239,7 @@ const theme = createTheme();
                     variant='h8'
                 >
                 Have you ever been told by a doctor that you had endometriosis?
+                <Typography></Typography>
                 </Typography>
                 <TextField id="endometriosis" label="Yes/No/Decline to Answer"
                 value={question9}
@@ -229,6 +252,7 @@ const theme = createTheme();
                     variant='h8'
                 >
                     Have you ever been told by a doctor that you had uterine fibroid tumors?
+                    <Typography></Typography>
                 </Typography>
                 <TextField id="uterine_fib" label="Yes/No/Decline to Answer"
                 value={question10}
@@ -241,6 +265,7 @@ const theme = createTheme();
                     variant='h8'
                 >
                      During the past three years, have you had a mammogram?
+                     <Typography></Typography>
 
                 </Typography>
                 <TextField id="mammo_history" label="Yes/No/Decline to Answer"
@@ -254,6 +279,7 @@ const theme = createTheme();
                     variant='h8'
                 >
                     During the past three years, have you had a pap smear?
+                    <Typography></Typography>
                 </Typography>
                 <TextField id="papsmear_history" label="Yes/No/Decline to Answer"
                 value={question12}
@@ -266,6 +292,7 @@ const theme = createTheme();
                     variant='h8'
                 >
                      During the past three years, have you had a pelvic examination?
+                     <Typography></Typography>
                 </Typography>
                 <TextField id="pelvic_history" label="Yes/No/Decline to Answer"
                 value={question13}
@@ -278,6 +305,7 @@ const theme = createTheme();
                     variant='h8'
                 >
                     During the past three years, have you had an ultrasound or scan of your ovaries?
+                    <Typography></Typography>
 
                 </Typography>
                 <TextField id="usound_history" label="Yes/No/Decline to Answer"
@@ -291,6 +319,7 @@ const theme = createTheme();
                     variant='h8'
                 >
                     During the past three years, have you had a blood test for ovarian cancer, for example CA-125?
+                    <Typography></Typography>
 
                 </Typography>
                 <TextField id="ca125_history" label="Yes/No/Decline to Answer"
@@ -304,6 +333,7 @@ const theme = createTheme();
                     variant='h8'
                 >
                     Have you ever been told by a doctor that you had benign or fibrocystic breast disease?
+                    <Typography></Typography>
 
                 </Typography>
                 <TextField id="bbd" label="Yes/No/Decline to Answer"
@@ -317,13 +347,21 @@ const theme = createTheme();
                     variant='h8'
                 >
                     Have you ever had one or both of your ovaries removed?
+                    <Typography>
                     Indicate EITHER:
+                    <Typography></Typography>
                     Ovaries Not Removed
+                    <Typography></Typography>
                     One Ovary - Partial
+                    <Typography></Typography>
                     One Ovary - Total
+                    <Typography></Typography>
                     Both Ovaries - Partial
+                    <Typography></Typography>
                     Both Ovaries - Total
+                    <Typography></Typography>
                     Don't Know
+                    </Typography>
                 </Typography>
                 <TextField id="ovariesr_f" label="" 
                 value={question17}
@@ -336,6 +374,7 @@ const theme = createTheme();
                     variant='h8'
                 >
                     Enter your age.
+                    <Typography></Typography>
                 </Typography>
                 <TextField id="age" label="#"
                 value={question18}
@@ -348,6 +387,7 @@ const theme = createTheme();
                     variant='h8'
                 >
                     What is your weight in pounds?
+                    <Typography></Typography>
                 </Typography>
                 <TextField id="weight_f" label="#"
                 value={question19}
@@ -360,6 +400,7 @@ const theme = createTheme();
                     variant='h8'
                 >
                     What is your height in inches?
+                    <Typography></Typography>
                 </Typography>
                 <TextField id="height_f" label="# inches" 
                 value={question20}
@@ -372,6 +413,7 @@ const theme = createTheme();
                     variant='h8'
                 >
                     Did you have ovaries at this height and weight?
+                    <Typography></Typography>
                 </Typography>
                 <TextField id="ovary_trial_flag" label="Yes/No"
                 value={question21}
@@ -384,14 +426,23 @@ const theme = createTheme();
                     variant='h8'
                 >
                     Over the past three months, how much have you beenbothered by abdominal, pelvic, or genital pain?
+                    <Typography>
                     Indicate either:
+                    <Typography></Typography>
                     1 - Not at all
+                    <Typography></Typography>
                     2 - A little
-                    3 A lot
-                    4 Somewhere in between a little and a lot
-                    7 Refuse to answer
-                    8 Not Ascertained
-                    9 Don't Know
+                    <Typography></Typography>
+                    3 - A lot
+                    <Typography></Typography>
+                    4 - Somewhere in between a little and a lot
+                    <Typography></Typography>
+                    7 - Refuse to answer
+                    <Typography></Typography>
+                    8 - Not Ascertained
+                    <Typography></Typography>
+                    9 - Don't Know
+                    </Typography>
                 </Typography>
                 <TextField id="PAIAPG3M_A" label="1/2/3/4/5/6/7/8/9"
                 value={question22}
@@ -404,14 +455,23 @@ const theme = createTheme();
                     variant='h8'
                 >
                     Over the last two weeks, how often have you been bothered by poor appetite or overeating?
+                    <Typography>
                     Indicate either:
+                    <Typography></Typography>
                     1 - Not at all
+                    <Typography></Typography>
                     2 - Several days
+                    <Typography></Typography>
                     3 - More than half the days
+                    <Typography></Typography>
                     4 - Nearly every day
+                    <Typography></Typography>
                     7 - Refused
+                    <Typography></Typography>
                     8 - Not Ascertained
+                    <Typography></Typography>
                     9 - Don't Know
+                    </Typography>
                 </Typography>
                 <TextField id="PHQ85_A" label="1/2/3/4/5/6/7/8/9"
                 value={question23}
@@ -425,15 +485,23 @@ const theme = createTheme();
                 >
                     Do you have difficulty walking or climbing steps? Would you say no difficulty,
                     some difficulty, a lot of difficulty, or you cannot do this at all?
+                    <Typography>
                     Indicate either:
+                    <Typography></Typography>
                     1 - No difficulty
+                    <Typography></Typography>
                     2 - Some difficulty
+                    <Typography></Typography>
                     3 - A lot of difficulty
+                    <Typography></Typography>
                     4 - Cannot do at all
+                    <Typography></Typography>
                     7 - Refused
+                    <Typography></Typography>
                     8 - Not Ascertained
+                    <Typography></Typography>
                     9 - Don't Know
-
+                    </Typography>
                 </Typography>
                 <TextField id="DIFF_A" label="1/2/3/4/5/6/7/8/9"
                 value={question24}
